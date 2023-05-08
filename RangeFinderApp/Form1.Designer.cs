56ae3cc31f6a415ca3228a6c7fe920b283007db7
Eugene Lamnek <eugene@geoit.com.au>
2023-05-08 20:01:37 +0930

    Add project files.

---
+++ RangeFinderApp/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace RangeFinderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRange = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMaxRange = new System.Windows.Forms.TextBox();
+            this.txtMeasureInterval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblRange
+            // 
+            this.lblRange.AutoSize = true;
+            this.lblRange.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRange.Location = new System.Drawing.Point(36, 32);
+            this.lblRange.Name = "lblRange";
+            this.lblRange.Size = new System.Drawing.Size(145, 159);
+            this.lblRange.TabIndex = 0;
+            this.lblRange.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LawnGreen;
+            this.groupBox1.Controls.Add(this.lblRange);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(27, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(585, 226);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtMaxRange
+            // 
+            this.txtMaxRange.Location = new System.Drawing.Point(276, 277);
+            this.txtMaxRange.Name = "txtMaxRange";
+            this.txtMaxRange.Size = new System.Drawing.Size(100, 26);
+            this.txtMaxRange.TabIndex = 2;
+            this.txtMaxRange.Text = "10";
+            // 
+            // txtMeasureInterval
+            // 
+            this.txtMeasureInterval.Location = new System.Drawing.Point(276, 327);
+            this.txtMeasureInterval.Name = "txtMeasureInterval";
+            this.txtMeasureInterval.Size = new System.Drawing.Size(100, 26);
+            this.txtMeasureInterval.TabIndex = 3;
+            this.txtMeasureInterval.Text = "250";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Maximum range to store (m)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Measurement frequency (ms)";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(454, 280);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(158, 60);
+            this.btnGo.TabIndex = 6;
+            this.btnGo.Text = "Measure!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 605);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMeasureInterval);
+            this.Controls.Add(this.txtMaxRange);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Submarine Range Finder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRange;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMaxRange;
+        private System.Windows.Forms.TextBox txtMeasureInterval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
